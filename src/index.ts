d26ccec14e124a347b1bc105b524823a6aa9cec2
Mike <broyden.sandbox3.dev@gmail.com>
2025-12-27 23:00:41 -0600

    added secrets manager to index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as github_helpers from './github';
 import * as github_classes from './github/github_classes';
 import * as github_types from './github/github_types';
 import { GitHubHelper } from './github/github_helper';
+import { SecretsManager } from './aws/secrets_manager';
 import { AwsEnvironmentManager } from './aws_environment_manager/aws_environment_manager';
 
-export { Num, encrypt, getProjects, GitHubHelper, github_classes, github_types, AwsEnvironmentManager }
+export { Num, encrypt, getProjects, GitHubHelper, github_classes, github_types, AwsEnvironmentManager, SecretsManager }
